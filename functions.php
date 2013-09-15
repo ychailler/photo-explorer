7e755edfc5c1276476291e547c8626c3a3a880f3
Yannick Chailler <yannick.chailler@gmail.com>
2013-09-15 20:24:11 +0200

    improve auth file reading

--- functions.php
+++ functions.php
@@ -1,15 +1,18 @@
 <?php
 
+$auth_list;
+
 function isAuthorized($path, $listImg) {
 	global $AUTH_MODE_ENABLED;
+	global $auth_list;
 	if(isset($AUTH_MODE_ENABLED) && $AUTH_MODE_ENABLED == 0){
 		return true;
 	}
 	$user = $_SERVER['REMOTE_USER'];
-	if(!isset($auths)) {
-		exec("/bin/grep -E '^$user:' auth.txt", $auths);
+	if(!isset($auth_list)) {
+		exec("/bin/grep -E '^$user:' auth.txt", $auth_list);
 	}
-	foreach ($auths as $i => $auth) {
+	foreach ($auth_list as $i => $auth) {
 		$auth=substr($auth,strlen($user)+1);
 		if(startsWith($path, $auth)) {
 			return true;
